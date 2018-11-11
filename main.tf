700e0c58b2247e4887bad2606ab1e1ed21b8e6bd
hifly <hifly@hiflys-MacBook-Pro.local>
2018-11-11 10:33:02 -0600

    updating tf

---
+++ main.tf
@@ -0,0 +1,63 @@
+// Configure the Google Cloud provider
+provider "google" {
+ credentials = "${file("${var.credentials}")}"
+ project     = "${var.gcp_project}" 
+ region      = "${var.region}"
+ zone         = "${var.zone}"
+}
+
+// Create VPC
+resource "google_compute_network" "vpc" {
+ name                    = "${var.name}-vpc"
+ auto_create_subnetworks = "false"
+ //project                 = "gansnew"
+}
+
+// Create Subnet
+resource "google_compute_subnetwork" "subnet" {
+ name          = "${var.name}-vpc"
+ ip_cidr_range = "${var.subnet_cidr}"
+ network       = "${var.name}-vpc"
+ depends_on    = ["google_compute_network.vpc"]
+ region      = "${var.region}"
+}
+// VPC firewall configuration
+resource "google_compute_firewall" "firewall" {
+  name    = "${var.name}-firewall"
+  //project = "${google_compute_network.vpc.project}"
+  network = "${google_compute_network.vpc.name}"
+  
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+// VM instance configuration
+resource "google_compute_instance" "vm-ins-01" {
+  name         = "${var.image}"
+  machine_type = "f1-micro"
+  depends_on    = ["google_compute_subnetwork.subnet"]
+
+  boot_disk {
+    initialize_params {
+      image = "${var.image}"
+    }
+  }
+
+  network_interface {
+    subnetwork = "${google_compute_network.vpc.name}"
+    access_config {}
+  }
+
+  scheduling {
+        preemptible = false
+        on_host_maintenance = "MIGRATE"
+        automatic_restart = true
+  }
+}
