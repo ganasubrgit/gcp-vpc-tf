9d2cb5d3636f177c84378051b72114225f5e6a2b
hifly <hifly@hiflys-MacBook-Pro.local>
2019-01-28 14:26:59 -0600

    updated startup

--- startup-script.sh
+++ startup-script.sh
@@ -5,7 +5,7 @@ setenforce 0
 firewall-cmd --permanent --add-port=9200/tcp
 firewall-cmd --permanent --add-port=9300/tcp
 firewall-cmd --reload   
-yum -y install java-1.8.0-openjdk
+yum -y install java-1.8.0-openjdk wget
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.2.4.rpm
 rpm -ivh elasticsearch-6.2.4.rpm
 systemctl daemon-reload
