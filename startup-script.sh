aa527aedcca6bf8cb2fa1dd15d369b57096f2e66
Ganapathy.Santhanam <Ganapathy.Santhanam@target.com>
2019-01-28 11:21:07 -0600

    update script for centos

--- startup-script.sh
+++ startup-script.sh
@@ -1,7 +1,14 @@
 #Sample ELasticSearch Install#
 #!/bin/bash
-sudo apt-get install default-jre wget
-wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add - 
-sudo apt-get install elasticsearch
+sysctl -w vm.swappiness=0
+setenforce 0
+firewall-cmd --permanent --add-port=9200/tcp
+firewall-cmd --permanent --add-port=9300/tcp
+firewall-cmd --reload   
+yum -y install java-1.8.0-openjdk
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.2.4.rpm
+rpm -ivh elasticsearch-6.2.4.rpm
+systemctl daemon-reload
+systemctl enable elasticsearch.service
 echo "network.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
-sudo service elasticsearch restart
+systemctl start elasticsearch.service
